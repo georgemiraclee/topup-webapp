3875dd2872a93ecd287788674c0118a929d1d812
georgemiraclee <georgemiracle2019@gmail.com>
2023-04-03 12:56:29 +0700

    beberapa bug

--- components/organisms/MainBanner/index.tsx
+++ components/organisms/MainBanner/index.tsx
@@ -7,12 +7,12 @@ const Header = () => (
         <div className="col-lg-6 col-12 my-auto">
           <p className="text-support text-lg color-palette-2">Halo gamers,</p>
           <h1 className="header-title color-palette-1 fw-bold">
-            Topup & Get
-            <span className="d-sm-inline d-none"> a</span>
-            <span className="d-sm-none d-inline"> a</span>
-            <span className="underline-blue"> New</span>
+            Dapatkan
+            <span className="d-sm-inline d-none"></span>
+            <span className="d-sm-none d-inline"></span>
+            <span className="underline-blue"> Pengalaman Baru </span>
             <br className="d-sm-block d-none" />
-            <span className="underline-blue">Experience</span> in Gaming
+            <span className="underline-blue"></span>Bermain Games
           </h1>
           <p className="mt-30 mb-40 text-lg color-palette-1">
             Kami menyediakan jutaan cara untuk membantu
@@ -25,14 +25,14 @@ const Header = () => (
               href="#feature"
               role="button"
             >
-              Get Started
+              Mulai 
             </a>
             <a
               className="btn-learn text-lg color-palette-1 my-auto text-center"
               href="/#"
               role="button"
             >
-              Learn More
+              Lebih banyak
             </a>
           </div>
         </div>
